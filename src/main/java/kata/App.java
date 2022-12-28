c78fe69d604f643007a0272400c79f47aea047c9
Lars Eckart <lars.eckart@hey.com>
2022-12-28 14:33:45 +0200

    updated Main

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -1,11 +1,13 @@
 package kata;
 
+import okhttp3.OkHttpClient;
+
 import java.io.IOException;
 
 class App {
 
     public static void main(String[] args) throws IOException {
-        PokeClient pokeClient = new PokeClient("https://pokeapi.co/api/v2/");
+        PokeClient pokeClient = new PokeClient("https://pokeapi.co/api/v2/", new OkHttpClient());
 
         for (int i = 1; i < 25; i++) {
             System.out.println(pokeClient.getPokemonNameAndLocations(i));
