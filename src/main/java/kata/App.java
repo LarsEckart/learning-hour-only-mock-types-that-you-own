1c43b90c232a2144f4d062aeb5a6cba705e25e26
Lars Eckart <lars.eckart@hey.com>
2023-01-03 11:20:53 +0200

    dont loop in main method

--- src/main/java/kata/App.java
+++ src/main/java/kata/App.java
@@ -9,8 +9,6 @@ class App {
     public static void main(String[] args) throws IOException {
         PokeClient pokeClient = new PokeClient(new PokeConfig("https://pokeapi.co/api/v2/", "anyApiKey"), new OkHttpClient());
 
-        for (int i = 1; i < 25; i++) {
-            System.out.println(pokeClient.getPokemonNameAndLocations(i));
-        }
+        System.out.println(pokeClient.getPokemonNameAndLocations(25));
     }
 }
