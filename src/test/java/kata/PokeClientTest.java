f95aafb5f1d6b6f67116d19a24189e78992437d1
Lars Eckart <lars.eckart@hey.com>
2023-01-03 15:46:27 +0200

    test everything at once, which is another test smell to talk about

--- src/test/java/kata/PokeClientTest.java
+++ src/test/java/kata/PokeClientTest.java
@@ -51,6 +51,7 @@ class PokeClientTest {
 
     @Test
     void testGetName() throws IOException {
+        ArgumentCaptor<Request> argumentCaptor = ArgumentCaptor.forClass(Request.class);
         given(okHttpClient.newCall(any())).willReturn(call);
         given(call.execute()).willReturn(response);
         given(response.isSuccessful()).willReturn(true);
@@ -64,22 +65,6 @@ class PokeClientTest {
         String actual = pokeClient.getName(25);
 
         assertThat(actual).isEqualTo("Pikachu");
-    }
-
-    @Test
-    void testGetNameMakesCorrectRequest() throws IOException {
-        ArgumentCaptor<Request> argumentCaptor = ArgumentCaptor.forClass(Request.class);
-        given(okHttpClient.newCall(any(Request.class))).willReturn(call);
-        given(call.execute()).willReturn(response);
-        given(response.isSuccessful()).willReturn(true);
-        given(response.body()).willReturn(responseBody);
-        given(responseBody.string()).willReturn("""
-                {
-                  "id": 25,
-                  "name": "pikachu"
-                }""");
-
-        pokeClient.getName(25);
 
         verify(okHttpClient).newCall(argumentCaptor.capture());
         Request request = argumentCaptor.getValue();
@@ -110,6 +95,7 @@ class PokeClientTest {
 
     @Test
     void testGetLocations() throws IOException {
+        ArgumentCaptor<Request> argumentCaptor = ArgumentCaptor.forClass(Request.class);
         given(okHttpClient.newCall(any())).willReturn(call);
         given(call.execute()).willReturn(response);
         given(response.isSuccessful()).willReturn(true);
@@ -175,75 +161,6 @@ class PokeClientTest {
         List<String> locations = pokeClient.getLocations(25);
 
         assertThat(locations).containsExactly("viridian-forest-area", "power-plant-area");
-    }
-
-    @Test
-    void testGetLocationsMakesCorrectRequest() throws IOException {
-        ArgumentCaptor<Request> argumentCaptor = ArgumentCaptor.forClass(Request.class);
-        given(okHttpClient.newCall(any())).willReturn(call);
-        given(call.execute()).willReturn(response);
-        given(response.isSuccessful()).willReturn(true);
-        given(response.body()).willReturn(responseBody);
-        given(responseBody.string()).willReturn("""
-                [
-                  {
-                    "location_area": {
-                      "name": "viridian-forest-area",
-                      "url": "https://pokeapi.co/api/v2/location-area/321/"
-                    },
-                    "version_details": [
-                      {
-                        "version": {
-                          "name": "red",
-                          "url": "https://pokeapi.co/api/v2/version/1/"
-                        }
-                      },
-                      {
-                        "version": {
-                          "name": "blue",
-                          "url": "https://pokeapi.co/api/v2/version/2/"
-                        }
-                      }
-                    ]
-                  },
-                  {
-                    "location_area": {
-                      "name": "power-plant-area",
-                      "url": "https://pokeapi.co/api/v2/location-area/330/"
-                    },
-                    "version_details": [
-                      {
-                        "version": {
-                          "name": "red",
-                          "url": "https://pokeapi.co/api/v2/version/1/"
-                        }
-                      },
-                      {
-                        "version": {
-                          "name": "leafgreen",
-                          "url": "https://pokeapi.co/api/v2/version/11/"
-                        }
-                      }
-                    ]
-                  },
-                  {
-                    "location_area": {
-                      "name": "hoenn-safari-zone-sw",
-                      "url": "https://pokeapi.co/api/v2/location-area/431/"
-                    },
-                    "version_details": [
-                      {
-                        "version": {
-                          "name": "ruby",
-                          "url": "https://pokeapi.co/api/v2/version/7/"
-                        }
-                      }
-                    ]
-                  }
-                ]""");
-
-        pokeClient.getLocations(25);
-
         verify(okHttpClient).newCall(argumentCaptor.capture());
         Request request = argumentCaptor.getValue();
         assertThat(request.url().toString()).isEqualTo("https://example.com/pokemon/25/encounters");
